3d90365405624f3521a4612f912950ea5e22f135
Mohamed-Kasali-Riswan <146856255+Mohamed-Kasali-Riswan@users.noreply.github.com>
2025-05-14 19:45:53 +0530

    Update TripleThreat.java

--- TripleThreat.java
+++ TripleThreat.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class candyCrush {
+public class TripleThreat {
 static Scanner sc=new Scanner(System.in);
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
